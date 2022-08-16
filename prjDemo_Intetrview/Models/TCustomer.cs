21d0fd503803c8896cf20cb93a61cdd7f7087f05
zgenez <yaj4604123@gmail.com>
2022-08-16 23:40:47 +0800

    專案 + Model  建置

---
+++ prjDemo_Intetrview/Models/TCustomer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace prjDemo_Intetrview.Models
+{
+    public partial class TCustomer
+    {
+        public int FCustomerId { get; set; }
+        public string FName { get; set; }
+        public string FPhone { get; set; }
+        public string FEmail { get; set; }
+        public string FAddress { get; set; }
+        public string FPassword { get; set; }
+        public string FCustomerImg { get; set; }
+    }
+}
---
+++ prjDemo_Intetrview/Models/TEmployee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace prjDemo_Intetrview.Models
+{
+    public partial class TEmployee
+    {
+        public int FEmployeeId { get; set; }
+        public string FName { get; set; }
+        public int? FAccess { get; set; }
+        public string FPosition { get; set; }
+        public string F隸屬 { get; set; }
+        public string FPassword { get; set; }
+        public string FEmployeeImg { get; set; }
+    }
+}
